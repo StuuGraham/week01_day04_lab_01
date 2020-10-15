31c0af559bd1b1c905b846efc17a1a6d1fa80154
Neale Johnston <neale@team-johnston.com>
2020-10-15 12:13:26 +0100

    One massive commit because I forgot to do it all the way through :)

---
+++ starting_point/src/friends.py
@@ -0,0 +1,49 @@
+def get_name(person):
+    return person["name"]
+
+
+def get_favourite_tv_show(person):
+    return person["favourites"]["tv_show"]
+
+
+def likes_to_eat(person, type_of_food):
+    for snack in person["favourites"]["snacks"]:
+        if snack == type_of_food:
+            return True
+    return False
+
+
+def add_friend(person, friend):
+    person["friends"].append(friend)
+
+
+def remove_friend(person, friend):
+    friend_index = person["friends"].index(friend)
+    person["friends"].pop(friend_index)
+
+
+def total_money(people):
+    money = 0
+    for person in people:
+        money += person["monies"]
+    return money
+
+def l_money(loaner, loanee, money):
+    loaner["monies"] -= money
+    loanee["monies"] += money
+
+
+def all_favourite_foods(list):
+    favourite_foods = []
+    for person in list:
+        for snack in person["favourites"]["snacks"]:
+            favourite_foods.append(snack)
+    return favourite_foods
+
+
+def find_no_friendends(list):
+    people_with_no_friends = []
+    for person in list:
+        if len(person["friends"]) == 0:
+            people_with_no_friends.append(person)
+    return people_with_no_friends
